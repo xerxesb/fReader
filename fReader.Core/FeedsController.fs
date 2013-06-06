e135091fdd646f10be73f4b9f6e4fdc9c8c03608
Xerxes Battiwalla <xerxesb@gmail.com>
2013-06-06 13:37:44 +1000

    Display feed content

--- fReader.Core/FeedsController.fs
+++ fReader.Core/FeedsController.fs
@@ -5,6 +5,7 @@ open System.Net
 open System.Web.Http
 open System.Web.Mvc
 open FSharpx
+open QDFeedParser
 
 type Feed = {
     id : string
@@ -16,19 +17,24 @@ module Data =
     let feeds = [ 
         { id = "1"; name = "xkcd"; url = "http://xkcd.com/rss.xml" }
         { id = "2"; name = "smbc"; url = "http://www.smbc-comics.com/rss.php" }
+        { id = "3"; name = "YC"; url = "http://feeds.feedburner.com/newsyc50" }
+        { id = "4"; name = "SO"; url = "http://blog.stackoverflow.com/feed" }
+        { id = "5"; name = "Troy Hunt"; url = "http://feeds.feedburner.com/TroyHunt" }
     ]
 
 type FeedsDataController() =
     inherit ApiController() 
     member this.Get() = Data.feeds
     member this.Get id = 
+        let feedFactory = QDFeedParser.HttpFeedFactory()
+        let notFound : IFeed = upcast Rss20Feed()
         let client = new System.Net.Http.HttpClient()
         Data.feeds |> List.tryFind (fun x -> x.id = id)
-                   |> Option.map (fun x -> client.GetStringAsync(x.url).Result) 
-                   |> Option.getOrElse "Can't find feed"
+                   |> Option.map (fun x -> feedFactory.CreateFeed(Uri(x.url)))
+                   |> Option.getOrElse notFound
 
 type FeedsController() =
     inherit Controller()
     member this.Details id = 
         let c = new FeedsDataController()
-        c.Get id
+        this.View(c.Get id)
